2978dc8b16a71798f1ed5bf7bc63b97272fe4a4d
Kenan Bandaliyev <kenanbanda2004@gmail.com>
2022-11-16 09:17:15 +0300

    Update project organisation

---
+++ compile_shaders.sh
@@ -0,0 +1,5 @@
+glslc ./shaders/point_light.vert -o ./shaders/point_light.frag.spv
+glslc ./shaders/simple_shader.vert -o ./shaders/simple_shader.vert.spv
+glslc ./shaders/simple_shader.frag -o ./shaders/simple_shader.frag.spv
+glslc ./shaders/point_light.vert -o ./shaders/point_light.vert.spv
+glslc ./shaders/point_light.vert -o ./shaders/point_light.frag.spv
