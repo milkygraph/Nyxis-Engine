2e1a57d116868609f98b75298d0963449e8afe39
BirsuYilmaz <ceren.birsu.yilmaz@gmail.com>
2022-11-27 21:49:02 +0300

    Texture Improvement 2

--- include/texture.hpp
+++ include/texture.hpp
@@ -1,26 +1,37 @@
 #pragma once
 
+#define STB_IMAGE_IMPLEMENTATION
 #include "stbimage/stb_image.h"
 #include "frameInfo.hpp"
-
+//createImageViews gonna be implemented to texture.cpp later
+//cleanup for the createTextureImageView and CreatTextureSampler gonna be implemented later
 namespace ve{
-class veTexture
-{
-    // Vulkan Texture Class
-    // This class is used to load textures into the Vulkan API
-    // It is used to load textures from files and create Vulkan textures from them
+    class veTexture
+    {
+        // Vulkan Texture Class
+        // This class is used to load textures into the Vulkan API
+        // It is used to load textures from files and create Vulkan textures from them
 
-    public:
-        veTexture();
-        ~veTexture();
+        public:
+            veTexture();
+            ~veTexture();
 
-        void loadTexture(std::string& filename, veDevice& device);
-        void createTextureImageView(veDevice& device);
-        void createTextureSampler(VkDevice& device);
+            void loadTexture(std::string& filename);
+            void createTextureImageView();
+            void createTextureSampler();
+            void createImage();
+            VkImageView createImageView();
+            void createImageViews();
 
-        VkImage textureImage;
-        VkDeviceMemory textureImageMemory;
-        VkImageView textureImageView;
-        VkSampler textureSampler;
-};
+            VkImage textureImage;
+            VkDeviceMemory textureImageMemory;
+            VkImageView textureImageView;
+            VkSampler textureSampler;
+            int texWidth;
+            int texHeight;
+            int texChannels;
+        private:
+            veDevice& device;
+    };
+
 }
--- src/texture.cpp
+++ src/texture.cpp
@@ -4,8 +4,11 @@
 namespace ve
 {
     veTexture::veTexture()
+     : device(device)
     {
-
+        int texWidth = 0;
+        int texHeight = 0;
+        int texChannels = 0;
     }
 
     veTexture::~veTexture()
@@ -13,13 +16,87 @@ namespace ve
 
     }
 
-    void veTexture::loadTexture(std::string& filename, veDevice& device)
+    void veTexture::loadTexture(std::string& filename)
     {
-        int texWidth, texHeight, texChannels;
-        // stbi_uc* pixels = stbi_load(filename.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+        
+        stbi_uc* pixels = stbi_load(filename.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
         VkDeviceSize imageSize = texWidth * texHeight * 4;
 
-        // if (!pixels)
-            // throw std::runtime_error("failed to load texture image!");
+        if (!pixels)
+            throw std::runtime_error("failed to load texture image!");
     }
+
+    void veTexture::createImage(){
+        VkImageCreateInfo imageInfo{};
+        imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+        imageInfo.imageType = VK_IMAGE_TYPE_2D;
+        imageInfo.extent.width = static_cast<uint32_t>(texWidth);
+        imageInfo.extent.height = static_cast<uint32_t>(texHeight);
+        imageInfo.extent.depth = 1;
+        imageInfo.mipLevels = 1;
+        imageInfo.arrayLayers = 1;
+        imageInfo.format = VK_FORMAT_R8G8B8A8_SRGB; //This might be changed, since some hardware might not support it
+        imageInfo.tiling = VK_IMAGE_TILING_OPTIMAL; 
+        imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        imageInfo.usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+        imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+        imageInfo.flags = 0;
+
+        if(vkCreateImage(device.device(), &imageInfo, nullptr, &textureImage)){
+           throw std::runtime_error("failed to create image!");
+        }
+
+    }
+
+    VkImageView veTexture::createImageView(){
+        VkImageViewCreateInfo viewInfo{};
+        viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+        viewInfo.image = textureImage;
+        viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+        viewInfo.format = VK_FORMAT_R8G8B8A8_SRGB;
+        viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        viewInfo.subresourceRange.baseMipLevel = 0;
+        viewInfo.subresourceRange.levelCount = 1;
+        viewInfo.subresourceRange.baseArrayLayer = 0;
+        viewInfo.subresourceRange.layerCount = 1;
+        viewInfo.components = {};
+
+        if(vkCreateImageView(device.device(), &viewInfo, nullptr, &textureImageView) != VK_SUCCESS){
+            throw std::runtime_error("failed to create texture image view!");
+        }
+    }
+    void veTexture::createTextureImageView(){
+        textureImageView = createImageView();
+    }
+
+    void veTexture::createTextureSampler(){
+        VkSamplerCreateInfo samplerInfo{};
+        samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+        samplerInfo.magFilter = VK_FILTER_LINEAR;
+        samplerInfo.minFilter = VK_FILTER_LINEAR;
+        samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        //Effects the performance
+        //samplerInfo.anisotropyEnable = VK_TRUE;
+        //samplerInfo.maxAnisotropy = ???;
+        VkPhysicalDeviceProperties properties{};
+        vkGetPhysicalDeviceProperties(device.device(), &properties);
+        samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
+        samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+        samplerInfo.unnormalizedCoordinates = VK_FALSE;
+        samplerInfo.compareEnable = VK_FALSE;
+        samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+        samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+        samplerInfo.mipLodBias= 0.0f;
+        samplerInfo.minLod = 0.0f;
+        samplerInfo.maxLod = 0.0f;
+
+        if(vkCreateSampler(device.device(), &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS){
+            throw std::runtime_error("failed to create texture sampler!");
+        }
+
+    }
+
 }
