75dfc8fff22212f6aa00e3827391d7dacc588138
itu-itis21-bandaliyev21 <bandaliyev21@itu.edu.tr>
2023-02-12 12:06:18 +0300

    Add assimp

--- include/frameInfo.hpp
+++ include/frameInfo.hpp
@@ -18,7 +18,7 @@ namespace ve
         glm::mat4 projection{1.f};
         glm::mat4 view{1.f};
         glm::mat4 inverseViewMatrix{1.f};
-        glm::vec4 ambientLightColor{1.f, 1.f, 1.f, .02f}; // w is intensity
+        glm::vec4 ambientLightColor{1.f, 1.f, 1.f, .2f}; // w is intensity
         PointLight pointLights[MAX_LIGHTS];
         int pointLightCount;
     };
