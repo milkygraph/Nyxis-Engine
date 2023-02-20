70aa4407dd37f613eee7da29675959efe663dd00
Kenan Bandaliyev <kenanbanda2004@gmail.com>
2023-02-20 14:22:26 +0400

    Update unix build script

--- UnixBuild.sh
+++ UnixBuild.sh
@@ -1,4 +1,4 @@
 cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S./ -B./build -G "Ninja"
-cmake --build ./build --config Debug --target Vulkan_Engine --
+cmake --build ./build --config Debug --target Nyxis_Engine --
 cd shaders
-./compile_shaders.sh
+./compile_shaders.sh glslc
