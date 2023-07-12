ce4457725f38c81f0ddb3b898f7bda176daee7dd
milkygraph <kenanbanda2004@gmail.com>
2023-07-12 16:35:50 +0300

    Remove Visual Studio folder

--- Visual Studio/libs/gli/gli/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/source/Nyxis-Engine/libs/gli/gli
-# Build directory: D:/source/Nyxis-Engine/Visual Studio/libs/gli/gli
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Visual Studio/libs/gli/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/source/Nyxis-Engine/libs/gli/test
-# Build directory: D:/source/Nyxis-Engine/Visual Studio/libs/gli/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("bug")
-subdirs("core")
